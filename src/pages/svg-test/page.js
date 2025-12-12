bb4b145d09b966b46a6f518c1d3b9a1d0a6ba643
ethan_pc <lochis.ethan@gmail.com>
2025-12-12 10:41:06 +0100

    Backup 10:41

--- src/pages/svg-test/page.js
+++ src/pages/svg-test/page.js
@@ -2,7 +2,13 @@ import { treeView } from "@/ui/Test";
 import { htmlToDOM } from "@/lib/utils.js";
 import template from "./template.html?raw";
 
+
 import { Animation } from "@/lib/animation.js";
+
+let M = {};
+let response = await fetch('/src/data/stars.json');
+M.data = await response.json();
+
 
 let C = {};
 
@@ -18,22 +24,22 @@ let V = {
 V.init = function () {
   V.rootPage = htmlToDOM(template);
   V.tree = new treeView();
-  // Animation.rotateElement(V.tree.getComprendre(), 3, 285);
-  // Animation.rotateElement(V.tree.getConcevoir(), 3, 235);
-  for (let i = 1; i < 7; i++) {
-    Animation.rotateElement(V.tree.getAC("150" + i), 4, 84 - i * 12);
+ 
+  // Animation ouverture des branches
+  for (let i = 1; i < 8; i++) {
+    // Rotation autour du centre
+    Animation.rotateElement(V.tree.getAC("150" + i), 4, 82 - i * 12); 
+    Animation.rotateElement(V.tree.getAC("140" + i), 4, 159 - i * 12.5);
+    Animation.rotateElement(V.tree.getAC("130" + i), 4, 230 - i * 12);
+    Animation.rotateElement(V.tree.getAC("120" + i), 4, 278 - i * 12);
+    Animation.rotateElement(V.tree.getAC("110" + i), 4, 351 - i * 12);
+    // Rotation des contenus
+    Animation.counterRotateElement(V.tree.getACcontent("150" + i), 4, -82 + i * 12);
+    Animation.counterRotateElement(V.tree.getACcontent("140" + i), 4, -159 + i * 12.5);
+    Animation.counterRotateElement(V.tree.getACcontent("130" + i), 4, -230 + i * 12);
+    Animation.counterRotateElement(V.tree.getACcontent("120" + i), 4, -278 + i * 12);
+    Animation.counterRotateElement(V.tree.getACcontent("110" + i), 4, -351 + i * 12);
   }
-  for (let i = 1; i < 7; i++) {
-    Animation.rotateElement(V.tree.getAC("140" + i), 4, 162 - i * 13);
-  }
-    for (let i = 1; i < 7; i++) {
-    Animation.rotateElement(V.tree.getAC("130" + i), 4, 240 - i * 13);
-  }
-
-  for (let i = 1; i < 7; i++) {
-    Animation.counterRotateElement(V.tree.getACcontent("130" + i), 4, -240 + i * 13);
-  }
-
   V.rootPage.querySelector('slot[name="svg"]').replaceWith(V.tree.dom());
   return V.rootPage;
 };
